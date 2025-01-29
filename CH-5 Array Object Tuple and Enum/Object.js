5d8796c708f60db93273460f44a19289f0dc0bc2
Zul Qarnain Afridi | MERN Stack Developer <beingzulqarnain@gmail.com>
2025-01-29 19:16:17 +0500

    A Complete TypeScript Notes For Beginner

---
+++ CH-5 Array Object Tuple and Enum/Object.js
@@ -0,0 +1,6 @@
+//Core Types In TS
+var user = {
+    name: "ali",
+    age: 43
+};
+console.log(user);
---
+++ CH-5 Array Object Tuple and Enum/Object.ts
@@ -0,0 +1,23 @@
+
+//Core Types In TS
+
+//Objects
+// const person:object = {
+//     name:"Arif",
+//     age:43,
+//     city:"Lahore",
+//     designation:"Software Engineer"
+// }
+
+// console.log(person);
+
+//Custom Object
+
+// type person2 = {name:string,age:number};
+
+// let user:person2 = {
+//     name:"ali",
+//     age:43
+// }
+
+// console.log(user);
