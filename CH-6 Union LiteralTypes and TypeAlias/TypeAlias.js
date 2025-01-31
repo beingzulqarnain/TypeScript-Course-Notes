a0f0f3f1dcf9349b5f5638bcc2ba95d27f93995f
Zul Qarnain Afridi | MERN Stack Developer <beingzulqarnain@gmail.com>
2025-01-31 18:39:11 +0500

    A Complete TypeScript Notes For Beginner

--- CH-6 Union LiteralTypes and TypeAlias/TypeAlias.js
+++ CH-6 Union LiteralTypes and TypeAlias/TypeAlias.js
@@ -1,14 +1,11 @@
 //CORE TYPES IN TS
-function combine(num1, num2, converstionType) {
-    var result;
-    if (typeof num1 === "number" && typeof num2 === "number" || converstionType === "as-number") {
-        result = +num1 + +num2;
-    }
-    else {
-        result = num1.toString() + num2.toString();
-    }
-    return result;
+var user = {
+    firstname: "Ali",
+    age: 21,
+    skills: ["C++", "C"]
+};
+console.log(user);
+function greet(user) {
+    console.log("I am ".concat(user.age));
 }
-var sum = combine(10, "20", "as-number");
-var cominedName = combine("Qarnain", " MernStack Developer", "as-string");
-console.log(sum, cominedName);
+greet(user);
--- CH-6 Union LiteralTypes and TypeAlias/TypeAlias.ts
+++ CH-6 Union LiteralTypes and TypeAlias/TypeAlias.ts
@@ -2,20 +2,44 @@
 
 //TYPE ALIAS IN TS - Assign specific string or number values to variables.
 
-type Combinable = number | string;
-type conversion = "as-number" | "as-string";
+// type Combinable = number | string;
+// type conversion = "as-number" | "as-string";
 
 
-function combine(num1:Combinable,num2:Combinable , converstionType:conversion){
-    let result;
-     if(typeof num1 === "number" && typeof num2 === "number" || converstionType === "as-number"){
-        result = +num1 + +num2;
-    }else{
-          result = num1.toString() + num2.toString();
-    }
-    return result;
- }
+// function combine(num1:Combinable,num2:Combinable , converstionType:conversion){
+//     let result;
+//      if(typeof num1 === "number" && typeof num2 === "number" || converstionType === "as-number"){
+//         result = +num1 + +num2;
+//     }else{
+//           result = num1.toString() + num2.toString();
+//     }
+//     return result;
+//  }
  
- const sum = combine(10,"20","as-number");
- const cominedName = combine("Qarnain"," MernStack Developer","as-string");
- console.log(sum,cominedName);
+//  const sum = combine(10,"20","as-number");
+//  const cominedName = combine("Qarnain"," MernStack Developer","as-string");
+//  console.log(sum,cominedName);
+
+
+
+
+// type User =  {
+//     firstname:string;
+//     age:number;
+//     skills:string[]
+// }
+// const user : User = {
+//     firstname:"Ali",
+//     age:21,
+//     skills:["C++","C"]
+// }
+
+// console.log(user);
+
+
+// function greet(user : User){
+//    console.log(`I am ${user.age}`);
+   
+// }
+
+greet(user);
