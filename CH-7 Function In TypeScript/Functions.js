a1f79cc0d9f0e95ece053d6c1c6be423450ca800
Zul Qarnain Afridi | MERN Stack Developer <beingzulqarnain@gmail.com>
2025-01-31 22:38:01 +0500

    A Complete TypeScript Notes For Beginner

--- CH-7 Function In TypeScript/Functions.js
+++ CH-7 Function In TypeScript/Functions.js
@@ -1,14 +1,10 @@
 //FUNCTIONS IN TYPESCRIPT
-  //Function return type 
-
-  // function add(num1:number,num2:number):number{
+//Function return type 
+// function add(num1:number,num2:number):number{
 //     return num1 + num2;
 // }
-
 // const sum = add(10,30);
 // console.log(sum);
-
-
 //Return null that we write void
 // function greet(name:string) : void {
 //     console.log(`Hi, I am ${name}`);   
@@ -18,22 +14,15 @@
 // const add = (num1:number,num2:number):number => {
 //     return num1 + num2 ;
 // }
-
 // let combineFunction : Function;
 // combineFunction = add;
-
 // console.log(combineFunction(23,44));
 // console.log(add);
-
-
 //Callback Function
-
-
-// function addHandle(num1:number,num2:number,cb:(n:number) => void){
-//     const result = num1 + num2 ;
-//     cb(result);
-// }
-
-// addHandle(10,40,(result : number) => {
-//     console.log(result);
-// })
+function addHandle(num1, num2, cb) {
+    var result = num1 + num2;
+    cb(result);
+}
+addHandle(10, 40, function (result) {
+    console.log(result);
+});
