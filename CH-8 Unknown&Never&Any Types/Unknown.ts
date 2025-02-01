05bffe3e2f5b7ca817a8acdcbb816bb6b856b4fe
Zul Qarnain Afridi | MERN Stack Developer <beingzulqarnain@gmail.com>
2025-02-01 21:03:17 +0500

    A Complete TypeScript Notes For Beginner

---
+++ CH-8 Unknown&Never&Any Types/Unknown.ts
@@ -0,0 +1,30 @@
+//CORE TYPES IN TS
+
+//unknown – Safer alternative to any, requiring type checks before use.
+//any – Disables type checking (not recommended).
+//never – Represents values that never occur (e.g., errors or infinite loops).
+
+//UNKNOWN
+
+// let userInput : unknown;
+// let userInput : any;
+// let userName  : string;
+
+
+// userInput = 10;
+// userName = userInput;
+// userName = "Ali Rehman";
+
+
+//never return type
+
+
+function generateError(message:string,code:number):never{
+       throw{
+        message:message,
+        statusCode:code,
+       }
+}
+
+const error = generateError("Internal Server Error",500);
+console.log(error);
